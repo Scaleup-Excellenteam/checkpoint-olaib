deb97ab363a96964bb989b37971ac81cad761b59
Ola Ibrahim <92628525+olaib@users.noreply.github.com>
2023-07-27 09:40:25 +0300

    starting step2: adding menu and some fixes

--- main.c
+++ main.c
@@ -10,26 +10,64 @@
 #define PHONE_LEN 11
 #define DB_INPUT_FILE "students.txt"
 #define ERROR_OPENING_FILE "Error opening file"
-#define INVALID_INPUT "Invalid input - level number must be between 1 and 10"
-int subject_index; // Global variable to store the subject index
+#define INVALID_INPUT "Invalid input, try again\n"
+#define ALLOC_ERROR "Memory allocation error.\n"
+#define MENU "Welcome to the Student Management System 2023\n"\
+             "1. Display all students\n"\
+             "2. Insert a new student\n"\
+             "3. Delete a student\n"\
+             "4. Update a student\n"\
+             "5. Search By first name and last name (display information)\n"\
+             "6. Exit\n"\
+             "Please enter your choice: 1 - 6\n"
+void clrscr();
+void updateStudent();
+void deleteStudent();
+void insertStudent();
+void displayStudents();
+void initDb();
+void displayMenu();
+void searchStudentByFnameByLName();
+void freeMemory();
+
+
+struct course {
+    int subject_id; // The subject ID is used as an index in the subject_map
+    int grade;
+};
 
 struct student {
     char fName[MAX_LEN];
     char lName[MAX_LEN];
     char phone[PHONE_LEN];
-    char* courses[NUM_COURSES];
-    int grades[NUM_COURSES];
     int level;
+    struct course courses[NUM_COURSES];
+    struct student* next;
+};
+
+struct subject_map {
+    char name[MAX_LEN];
 };
 
 struct school {
-    struct student DB[NUM_LEVELS][NUM_CLASSES];
+    struct student* DB[NUM_LEVELS][NUM_CLASSES];
     int num_students;
+    struct subject_map subjects[NUM_COURSES];
 };
 
 static struct school S;
 
-void init_DB(struct school* sch) {
+int main() {
+    initDb();
+    displayMenu();
+    return EXIT_SUCCESS;
+}
+
+void clrscr() {
+    system("@cls||clear");
+}
+
+void initDb() {
     FILE* file = fopen(DB_INPUT_FILE, "r");
     if (file == NULL) {
         printf(ERROR_OPENING_FILE);
@@ -41,185 +79,247 @@ void init_DB(struct school* sch) {
     int grades[NUM_CLASSES];
 
     while (fscanf(file, "%s %s %s %d %d", fName, lName, phone, &level, &class_id) == 5) {
-        struct student new_student;
-        strcpy(new_student.fName, fName);
-        strcpy(new_student.lName, lName);
-        strcpy(new_student.phone, phone);
+        struct student* new_student = malloc(sizeof(struct student));
+        if (new_student == NULL) {
+            printf("Memory allocation error.\n");
+            return;
+        }
+
+        strcpy(new_student->fName, fName);
+        strcpy(new_student->lName, lName);
+        strcpy(new_student->phone, phone);
+        new_student->level = level;
 
         for (int i = 0; i < NUM_CLASSES; i++) {
-            fscanf(file, "%d", &grades[i]);
-            new_student.grades[i] = grades[i];
+            if (fscanf(file, "%d", &grades[i]) != 1) {
+                printf("Invalid input in file.\n");
+                free(new_student);
+                return;
+            }
+            new_student->courses[i].subject_id = i;
+            new_student->courses[i].grade = grades[i];
         }
-        new_student.level = level;
 
+        new_student->next = S.DB[level - 1][class_id - 1];
         S.DB[level - 1][class_id - 1] = new_student;
         S.num_students++;
     }
 
+    // Initializing the subject_map 
+    const char* subjectNames[NUM_COURSES] = {
+        "Mathematics", "English", "Science", "History", "Internet Programming",
+        "Physics", "Chemistry", "Biology", "Geography", "Literature",
+        "Physical Education", "Art", "Music","Economics", "Python",
+        "Foreign Language", "Health", "Cpp", "Psychology", "Sociology"
+    };
+
+
+    struct subject_map* current_subject = S.subjects;
+
     fclose(file);
 }
 
-void display_students(struct school* sch) {
-    printf("List of Students:\n");
-    for (int level = 0; level < NUM_LEVELS; level++) {
-        for (int class_id = 0; class_id < NUM_CLASSES; class_id++) {
-            struct student* current_student = &sch->DB[level][class_id];
-            if (current_student->courses[0] != NULL) {
-                printf("Name: %s %s, Phone: %s, Level: %d, Class: %d\n",
-                    current_student->fName, current_student->lName,
-                    current_student->phone, level + 1, class_id + 1);
-            }
-        }
+void displaySubjects(struct student* student) {
+    printf("Subjects:\n");
+    for (int i = 0; i < NUM_COURSES; i++) {
+        int subject_id = student->courses[i].subject_id;
+        printf("%s: %d\n", S.subjects[subject_id].name, student->courses[i].grade);
     }
 }
 
-void add_student(struct school* sch) {
+void displayStudents() {
+    printf("=========== List of Students: ===========\n");
+    for (int level = 0; level < NUM_LEVELS; level++) {
+        for (int class_id = 0; class_id < NUM_CLASSES; class_id++) {
+            struct student* current_student = S.DB[level][class_id];
+            while (current_student != NULL) {
+                printf("First Name: %s, Last Name: %s\n",
+                    current_student->fName, current_student->lName);
+                printf("Phone: %s\n", current_student->phone);
+                printf("Level: %d\n", current_student->level);
+                displaySubjects(current_student);
+                printf("\n");
+                current_student = current_student->next;
+            }
+        }
+    }
+    printf("=========================================\n");
+}
+
+void insertStudent() {
     char fName[MAX_LEN], lName[MAX_LEN], phone[PHONE_LEN];
     int level, class_id;
+    int grades[NUM_CLASSES];
 
-    printf("Enter first name: ");
+    printf("Enter the first name of the student: ");
     scanf("%s", fName);
-
-    printf("Enter last name: ");
+    printf("Enter the last name of the student: ");
     scanf("%s", lName);
-
-    printf("Enter phone number: ");
+    printf("Enter the phone number of the student: ");
     scanf("%s", phone);
-
-    printf("Enter level (1-10): ");
+    printf("Enter the level of the student: ");
     scanf("%d", &level);
-    if (level < 1 || level > NUM_LEVELS) {
-        printf(INVALID_INPUT);
-        return;
-    }
-
-    printf("Enter class (1-20): ");
+    printf("Enter the class id of the student: ");
     scanf("%d", &class_id);
-    if (class_id < 1 || class_id > NUM_CLASSES) {
-        printf(INVALID_INPUT);
+
+    struct student* new_student = malloc(sizeof(struct student));
+    if (new_student == NULL) {
+        printf(ALLOC_ERROR);
         return;
     }
 
-    struct student new_student;
-    strcpy(new_student.fName, fName);
-    strcpy(new_student.lName, lName);
-    strcpy(new_student.phone, phone);
+    strcpy(new_student->fName, fName);
+    strcpy(new_student->lName, lName);
+    strcpy(new_student->phone, phone);
+    new_student->level = level;
 
-    // Initializing courses array to NULL
-    for (int i = 0; i < NUM_COURSES; i++) {
-        new_student.courses[i] = NULL;
+    for (int i = 0; i < NUM_CLASSES; i++) {
+        printf("Enter the grade of the student in subject %d: ", i + 1);
+        scanf("%d", &grades[i]);
+        new_student->courses[i].subject_id = i;
+        new_student->courses[i].grade = grades[i];
     }
 
+    new_student->next = S.DB[level - 1][class_id - 1];
     S.DB[level - 1][class_id - 1] = new_student;
     S.num_students++;
-    printf("Student added successfully!\n");
 }
 
-
-int compare_grades(const void* a, const void* b) {
-    const struct student* studentA = (const struct student*)a;
-    const struct student* studentB = (const struct student*)b;
-    return studentB->grades[subject_index] - studentA->grades[subject_index];
-}
-
-void get_top_students(int subject_idx) {
-    subject_index = subject_idx;
-
-    struct student all_students[NUM_LEVELS * NUM_CLASSES];
-    int num_students = 0;
+void deleteStudent() {
+    char fName[MAX_LEN], lName[MAX_LEN];
+    printf("Enter the first name of the student: ");
+    scanf("%s", fName);
+    printf("Enter the last name of the student: ");
+    scanf("%s", lName);
 
     for (int level = 0; level < NUM_LEVELS; level++) {
         for (int class_id = 0; class_id < NUM_CLASSES; class_id++) {
-            struct student* current_student = &S.DB[level][class_id];
-            if (current_student->courses[0] != NULL) {
-                all_students[num_students++] = *current_student;
+            struct student* current_student = S.DB[level][class_id];
+            struct student* prev_student = NULL;
+            while (current_student != NULL) {
+                if (strcmp(current_student->fName, fName) == 0 &&
+                    strcmp(current_student->lName, lName) == 0) {
+                    if (prev_student != NULL)
+                        prev_student->next = current_student->next;
+                    else
+                        S.DB[level][class_id] = current_student->next;
+
+                    free(current_student);
+                    S.num_students--;
+                    return;
+                }
+                prev_student = current_student;
+                current_student = current_student->next;
             }
         }
     }
+    printf("Student not found.\n");
+}
 
-    // Sort the students based on grades in the specific subject
-    qsort(all_students, num_students, sizeof(struct student), compare_grades);
+void updateStudent() {
+    char fName[MAX_LEN], lName[MAX_LEN];
+    printf("Enter the first name of the student: ");
+    scanf("%s", fName);
+    printf("Enter the last name of the student: ");
+    scanf("%s", lName);
 
-    int grade_counter[NUM_LEVELS] = { 0 };
-    printf("Top Ten Students in Each Grade for Subject %d:\n", subject_index + 1);
-    for (int i = 0; i < num_students; i++) {
-        int level = all_students[i].level - 1;
-        if (grade_counter[level] < 10) {
-            printf("%s %s - Grade: %d\n", all_students[i].fName, all_students[i].lName,
-                all_students[i].grades[subject_index]);
-            grade_counter[level]++;
+    for (int level = 0; level < NUM_LEVELS; level++) {
+        for (int class_id = 0; class_id < NUM_CLASSES; class_id++) {
+            struct student* current_student = S.DB[level][class_id];
+            while (current_student != NULL) {
+                if (strcmp(current_student->fName, fName) == 0 &&
+                    strcmp(current_student->lName, lName) == 0) {
+                    printf("Enter the new first name of the student: ");
+                    scanf("%s", current_student->fName);
+                    printf("Enter the new last name of the student: ");
+                    scanf("%s", current_student->lName);
+                    printf("Enter the new phone number of the student: ");
+                    scanf("%s", current_student->phone);
+                    printf("Enter the new level of the student: ");
+                    scanf("%d", &current_student->level);
+
+                    for (int i = 0; i < NUM_CLASSES; i++) {
+                        printf("Enter the new grade of the student in subject %d: ", i + 1);
+                        scanf("%d", &current_student->courses[i].grade);
+                    }
+                    return;
+                }
+                current_student = current_student->next;
+            }
+        }
+    }
+    printf("Student not found.\n");
+}
+
+void searchStudentByFnameByLName() {
+    char fName[MAX_LEN], lName[MAX_LEN];
+    printf("Enter the first name of the student: ");
+    scanf("%s", fName);
+    printf("Enter the last name of the student: ");
+    scanf("%s", lName);
+
+    for (int level = 0; level < NUM_LEVELS; level++) {
+        for (int class_id = 0; class_id < NUM_CLASSES; class_id++) {
+            struct student* current_student = S.DB[level][class_id];
+            while (current_student != NULL) {
+                if (strcmp(current_student->fName, fName) == 0 &&
+                    strcmp(current_student->lName, lName) == 0) {
+                    printf("First Name: %s, Last Name: %s\n",
+                        current_student->fName, current_student->lName);
+                    printf("Phone: %s\n", current_student->phone);
+                    printf("Level: %d\n", current_student->level);
+                    displaySubjects(current_student);
+                    printf("\n");
+                    return;
+                }
+                current_student = current_student->next;
+            }
+        }
+    }
+    printf("Student not found.\n");
+}
+
+void freeMemory() {
+    for (int level = 0; level < NUM_LEVELS; level++) {
+        for (int class_id = 0; class_id < NUM_CLASSES; class_id++) {
+            struct student* current_student = S.DB[level][class_id];
+            while (current_student != NULL) {
+                struct student* temp = current_student;
+                current_student = current_student->next;
+                free(temp);
+            }
         }
     }
 }
-void get_candidates_for_departure() {
-}
 
-void calculate_average_grades() {
-}
-
-void encrypt_decrypt_data(bool encrypt) {
-}
-
-void display_menu() {
-    printf("\nMain Menu\n");
-    printf("1. Admission of a new student\n");
-    printf("2. Query the top ten students in each grade for a particular subject\n");
-    printf("3. Query students who are candidates for departure\n");
-    printf("4. Calculation of average per course per layer\n");
-    printf("5. Encrypt student information\n");
-    printf("6. Decrypt student information\n");
-    printf("7. Save data to file (optional)\n");
-    printf("8. Exit the program\n");
-}
-
-int main() {
-    struct school sch = { 0 };
-
-    // Load data from the file to the DB
-    init_DB(&sch);
-
-    // Display all students
-    display_students(&sch);
-
-    int choice;
-    do {
-        display_menu();
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
+void displayMenu() {
+    char choice;
+    while (true) {
+        //clrscr();
+        printf(MENU);
+        scanf(" %c", &choice);
         switch (choice) {
-        case 1:
-            add_student(&sch);
+        case '1':
+            displayStudents();
             break;
-        case 2: {
-            int subject_index;
-            printf("Enter the subject index (0-%d)", NUM_COURSES - 1);
-            scanf("%d", &subject_index);
-            get_top_students(subject_index);
+        case '2':
+            insertStudent();
             break;
-        }
-        case 3:
-            get_candidates_for_departure();
+        case '3':
+            deleteStudent();
             break;
-        case 4:
-            calculate_average_grades();
+        case '4':
+            updateStudent();
             break;
-        case 5:
-            encrypt_decrypt_data(true); // Encrypt data
+        case '5':
+            searchStudentByFnameByLName();
             break;
-        case 6:
-            encrypt_decrypt_data(false); // Decrypt data
-            break;
-        case 7:
-            // Save data to the file
-            break;
-        case 8:
-            printf("Exiting the program. Goodbye!\n");
-            break;
+        case '6':
+            freeMemory();
+            exit(EXIT_SUCCESS);
         default:
-            printf("Invalid choice. Please try again.\n");
+            printf(INVALID_INPUT);
+            break;
         }
-    } while (choice != 8);
-
-    return 0;
+    }
 }
