9ad1bd44f1508525def1413de82930791605bdd4
Ola Ibrahim <92628525+olaib@users.noreply.github.com>
2023-07-30 22:29:13 +0300

    some fixes

--- kernel-firewall/traffic_logger.c
+++ kernel-firewall/traffic_logger.c
@@ -5,21 +5,22 @@
 #include <linux/netfilter_ipv4.h>
 #include <linux/ip.h>
 #include <linux/tcp.h>
+#include <string.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Ola Ibrahim");
-MODULE_DESCRIPTION("A simple traffic logger kernel module");
+MODULE_DESCRIPTION("A simple Traffic logger kernel module");
 MODULE_VERSION("1.00");
 
-#define HOOK_MSG_SUCCESS "logger traffic success successfully\n"
-#define HOOK_MSG_FAIL "logger traffic failed\n")
+#define HOOK_MSG_SUCCESS "Traffic logger: Hook registered successfully\n"
+#define HOOK_MSG_FAIL "Traffic logger: Hook registration failed\n"
 #define IP_REGEX "^([0-9]{1,3}\\.){3}[0-9]{1,3}$"
 
 struct nf_hook_ops HOOKS_OPTIONS = { 0 }; // for configuring hook options
 
 static unsigned int log_incoming_traffic(void* priv, struct sk_buff* skb, const struct nf_hook_state* state);
 
-static int __init ftraffic_logger_init(void);
+static int __init traffic_logger_init(void);
 static void __exit traffic_logger_exit(void);
 
 /*
@@ -32,14 +33,14 @@ static void __exit traffic_logger_exit(void);
 static unsigned int log_incoming_traffic(void* priv, struct sk_buff* skb, const struct nf_hook_state* state)
 {
     struct iphdr* iph; // IP header struct
-    struct icmphdr* tcph; // Tcp header struct
+    struct tcphdr* tcph; // TCP header struct
     if (!skb) return NF_ACCEPT; // if no packet, accept it
 
     iph = ip_hdr(skb); // get IP header from packet
 
-    if (iph->protocol == IPPROTO_TCP) // if protocol is ICMP
+    if (iph->protocol == IPPROTO_TCP) // if protocol is TCP
     {
-        tcph = icmp_hdr(skb); // get ICMP header from packet
+        tcph = tcp_hdr(skb); // get TCP header from packet
         printk(KERN_INFO "Incoming traffic: %pI4: %d --> %pI4: %d\n",
             &iph->saddr, ntohs(tcph->source),
             &iph->daddr, ntohs(tcph->dest)
@@ -49,12 +50,12 @@ static unsigned int log_incoming_traffic(void* priv, struct sk_buff* skb, const 
     return NF_ACCEPT; // accept packet
 }
 
-static int __init firewall_init(void)
+static int __init traffic_logger_init(void)
 {
     printk(KERN_INFO "Incoming traffic initializing ...\n");
 
     HOOKS_OPTIONS.hook = log_incoming_traffic; // set hook function
-    HOOKS_OPTIONS.PF = NFPROTP.IPV4; // set protocol family
+    HOOKS_OPTIONS.pf = NFPROTO_IPV4; // set protocol family
     HOOKS_OPTIONS.hooknum = NF_INET_LOCAL_IN; // set hook number
     HOOKS_OPTIONS.priority = NF_IP_PRI_FIRST; // set hook priority
 
@@ -70,5 +71,7 @@ static void __exit traffic_logger_exit(void)
     printk(KERN_INFO "Traffic_logger: Exiting...\n");
 }
 
-module_init(firewall_init);
-module_exit(firewall_exit);
+module_init(traffic_logger_init);
+module_exit(traffic_logger_exit);
+
+
